0ac35f1dd705079a042978b4d8f8b3afe2e4038e
says33 <says.rodriguez@gmail.com>
2015-08-01 16:21:07 -0500

    coding #1 se crea la interface con la creacion del metod uploadProductValuarte

---
+++ src/main/groovy/com/team/one/service/uploadProducts/UploadProductsService.groovy
@@ -0,0 +1,9 @@
+package com.team.one.service.uploadProducts
+
+import org.springframework.web.multipart.MultipartFile
+
+public interface UploadProductsService {
+
+  Integer uploadProductsInValuarte(MultipartFile file)
+
+}
