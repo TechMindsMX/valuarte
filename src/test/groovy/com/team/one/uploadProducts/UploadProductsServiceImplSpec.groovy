c4763d2accef6dd1f86956d4ec24f6f5ed9b13a9
says33 <says.rodriguez@gmail.com>
2015-08-02 22:16:16 -0500

    coding #2 se ajusta la prueba para realizar la creacion del mockMultiPartFile

--- src/test/groovy/com/team/one/uploadProducts/UploadProductsServiceImplSpec.groovy
+++ src/test/groovy/com/team/one/uploadProducts/UploadProductsServiceImplSpec.groovy
@@ -6,6 +6,7 @@ import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 import org.springframework.mock.web.MockMultipartFile
+import java.io.InputStream
 
 import com.team.one.service.uploadProducts.UploadProductsServiceImpl
 
@@ -15,8 +16,8 @@ class UploadProductsServiceImplSpec extends Specification {
 
   void "obtains all values of excel"() {
     when:
-      MockMultipartFile file = new MockMultipartFile("/Users/says/Documents/mabe.xls")
-      println file.dump()
+      FileInputStream inputFile = new FileInputStream( "/Users/says/Documents/mabe.xls")
+      MockMultipartFile file = new MockMultipartFile("file", "NameOfTheFile", "multipart/form-data", inputFile)
       def count = uploadP.uploadProductsInValuarte(file)
     then:
       count == 4
