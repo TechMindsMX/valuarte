aa18d85eda4e4e49f2403db3ab2e983e6df5516c
josdem <joseluis.delacruz@gmail.com>
2015-08-12 11:40:49 -0500

    #8 Actualizando los valores a la nueva version del excel

--- src/main/groovy/com/team/one/service/CallServiceImpl.groovy
+++ src/main/groovy/com/team/one/service/CallServiceImpl.groovy
@@ -28,9 +28,9 @@ class CallServiceImpl implements CallService {
       String descripcion = "${params[2]}-${params[4]}-${params[5]}"
       propertiesCreateProduct.userId = 1
       propertiesCreateProduct.name = name.toString()
-      propertiesCreateProduct.subcategory = 41
+      propertiesCreateProduct.subcategory = params[1]
       propertiesCreateProduct.description = descripcion.toString()
-      propertiesCreateProduct.photos = "1.jpg"
+      propertiesCreateProduct.photos = params[2].toString()
       propertiesCreateProduct.token = token
       propertiesCreateProduct.status = 0
       RestTemplate restTemplate = new RestTemplate()
