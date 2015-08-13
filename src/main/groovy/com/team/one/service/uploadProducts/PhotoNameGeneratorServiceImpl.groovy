666e5f5cf999a3914004d4a4b243bc0901eed17a
josdem <joseluis.delacruz@gmail.com>
2015-08-12 20:32:57 -0500

    #8 Completing test photo name generator

--- src/main/groovy/com/team/one/service/uploadProducts/PhotoNameGeneratorServiceImpl.groovy
+++ src/main/groovy/com/team/one/service/uploadProducts/PhotoNameGeneratorServiceImpl.groovy
@@ -5,9 +5,9 @@ class PhotoNameGeneratorServiceImpl implements PhotoNameGeneratorService {
   String getNames(String sku, Integer photoNumber) {
     def sb = new StringBuilder()
     (0..photoNumber-1).each{ number ->
-      sb.append("${sku}-${number+1},")
+      sb.append("${sku}-${number+1}.jpg,")
     }
-    sb.toString()
+    sb.toString()[0..-2]
   }
 
 }
