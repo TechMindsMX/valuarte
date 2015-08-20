7ef77ac1d8bc06becf062858af2bd9a1ad4fc1b9
says <says.rodriguez@gmail.com>
2015-08-20 11:45:49 -0500

    coding #14 se agregan los metodos para poder utilizar los elementos del landing page

--- src/main/groovy/com/team/one/controller/HomeController.groovy
+++ src/main/groovy/com/team/one/controller/HomeController.groovy
@@ -8,7 +8,41 @@ class HomeController {
 
     @RequestMapping("/")
     String getHomePage() {
-      return "home/index"
+      return "home/home"
     }
 
+	@RequestMapping("/who")
+    String getWhoPage() {
+      return "home/who"
+    }
+
+    @RequestMapping("/products")
+    String getProductsPage() {
+      return "home/products"
+    }
+
+    @RequestMapping("/mision")
+    String getMisionPage() {
+      return "home/mision"
+    }
+
+    @RequestMapping("/benefits")
+    String getBenefitsPage() {
+      return "home/benefits"
+    }
+
+    @RequestMapping("/contact")
+    String getContactPage() {
+      return "home/contact"
+    }
+
+    @RequestMapping("/education")
+    String getEducationPage() {
+      return "home/education"
+    }
+
+    @RequestMapping("/privacy")
+    String getPrivacyPage() {
+      return "home/privacy"
+    }
 }
