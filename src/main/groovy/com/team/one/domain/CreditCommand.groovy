59b6e453505d10e9f96923f063f13da5453dd754
says <says.rodriguez@gmail.com>
2015-09-09 11:15:01 -0500

    #38 se agrega el command para el salvado de el formularion de credito

---
+++ src/main/groovy/com/team/one/domain/CreditCommand.groovy
@@ -0,0 +1,45 @@
+package com.team.one.domain
+
+import com.team.one.domain.enums.*
+
+class CreditCommand {
+
+  String montoPrestamo
+  String nombre
+  String apellidoPaterno
+  String apellidoMaterno
+  Date fechaNacimiento
+  Integer edad
+  EstadoCivil estadoCivil
+  RegimenMatrimonial regimen
+  GradoMaximoEstudios grado
+  Genero genero
+  TipoIdentificacion identificacion
+  String rfc
+  String fiel
+  String curp
+  String claveElector
+  String telefono
+  String celular
+  String telefonoOficina
+  String email
+  String nacionalidad
+  TipoVivienda vivienda
+  String tiempoResidencia
+  String RentaCosto
+  String hipotecaCosto
+  Integer dependientes
+  String codigoPostal
+  String calleYNumero
+  String colonia
+  String delegacionMunicipio
+  String ciudadOEstado
+  String pais
+  String referenciaDomicilio
+  String profecion
+  String empresa
+  String giroActividad
+  TipoContrato contrato
+  Date fechaIngreso
+
+}
