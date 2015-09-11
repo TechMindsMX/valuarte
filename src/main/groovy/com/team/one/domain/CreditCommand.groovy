bdc280955f04a93baa89089bf862f456b260004a
says <says.rodriguez@gmail.com>
2015-09-11 12:29:40 -0500

    #38 se agregan los elementos faltantes en el command para la form de la soliciud de pretamo

--- src/main/groovy/com/team/one/domain/CreditCommand.groovy
+++ src/main/groovy/com/team/one/domain/CreditCommand.groovy
@@ -5,6 +5,7 @@ import com.team.one.domain.enums.*
 class CreditCommand {
 
   String montoPrestamo
+  //Primera seccion
   String nombre
   String apellidoPaterno
   String apellidoMaterno
@@ -47,5 +48,80 @@ class CreditCommand {
   String domicilioEmpresa
   String ciudadEstadoEmpresa
   String telefonoEmpresa
+  //Segunda seccion
+  String nombreAval
+  String apellidoPaternoAval
+  String apellidoMaternoAval
+  Date fechaNacimeintoAval
+  TipoIdentificacion identificacionAval
+  String identificacionOtroAval
+  String nombreEmpresaAval
+  String giroActividadAval
+  String telefonoTrabajoAval
+  TipoContrato contratoAval
+  String sueldoMensualAval
+  String domicilioEmpresaAval
+  String ciudadEstadoAval
+  //Tercera seccion
+  String nombreCompletoRefFamiliar
+  String parentescoFamiliar
+  String telefonoFamiliar
+  String domicilioFamiliar
+  String nombreCompletoRefLaboral
+  String parentescoLaboral
+  String telefonoLaboral
+  String domicilioLaboral
+  String nombreCompletoRefPersonal1
+  String parentescoPersonal1
+  String telefonoPersonal1
+  String domicilioPersonal1
+  String nombreCompletoRefPersonal2
+  String parentescoPersonal2
+  String telefonoPersonal2
+  String domicilioPersonal2
+  //Cuarta seccion
+  Boolean creditoActual
+  Boolean avalCreditoTercero
+  String tipoCreditoDeuda1
+  String institucionDeuda1
+  String numeroCreditoDeuda1
+  Date fechaAperturaDeuda1
+  Date fechaTerminoDeuda1
+  String montoOtorgadoDeuda1
+  String tipoCreditoDeuda2
+  String institucionDeuda2
+  String numeroCreditoDeuda2
+  Date fechaAperturaDeuda2
+  Date fechaTerminoDeuda2
+  String montoOtorgadoDeuda2
+  String tipoCreditoDeuda3
+  String institucionDeuda3
+  String numeroCreditoDeuda3
+  Date fechaAperturaDeuda3
+  Date fechaTerminoDeuda3
+  String montoOtorgadoDeuda3
+  String tipoCreditoDeuda4
+  String institucionDeuda4
+  String numeroCreditoDeuda4
+  Date fechaAperturaDeuda4
+  Date fechaTerminoDeuda4
+  String montoOtorgadoDeuda4
+  String nombreBeneficiario
+  //Quita seccion
+  String tipoSeguro
+  String nombreBeneficiario1
+  String nombreBeneficiario2
+  String domicilioBeneficiario1
+  String domicilioBeneficiario2
+  //Septima seccion
+  FuenteIngresos fuenteIngresos
+  String fuenteIngresosOtros
+  String profesionOcupacion
+  String actividadGiroNegocio
+  RangoMensual rangoMensual
+  OrigenRecursos origenRecursos
+  InstrumentoPago instrumentoPago
+
+
 
 }
