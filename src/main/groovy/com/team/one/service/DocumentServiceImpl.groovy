391d8adac483e7fd711224ad1bc2e4e306eee51b
says <says.rodriguez@gmail.com>
2015-09-18 12:22:32 -0500

    #35 se crea el servicio para realizar la generacion de los documentos del kit de salida

---
+++ src/main/groovy/com/team/one/service/DocumentServiceImpl.groovy
@@ -0,0 +1,19 @@
+package com.team.one.service
+
+import org.springframework.stereotype.Service
+import org.springframework.beans.factory.annotation.*
+import org.springframework.web.client.RestTemplate
+import org.springframework.util.*
+import com.google.gson.Gson
+import org.springframework.http.*
+
+@Service
+class DocumentServiceImpl implements DocumentService {
+
+	def createDocumentStartedKit(def userComand) {
+		def userInfo //= Aqui debera existir la busqueda de los datos del usuario para llenar los campos del pdf
+
+		
+	}
+	
+}
