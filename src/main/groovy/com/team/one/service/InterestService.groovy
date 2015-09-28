aff6ce91d87274ac1d66727c1b19d37f6b7492a4
josdem <joseluis.delacruz@gmail.com>
2015-09-28 11:35:25 -0500

    #62 Agregando servicio que calcula el interes basado en el capital

---
+++ src/main/groovy/com/team/one/service/InterestService.groovy
@@ -0,0 +1,7 @@
+package com.team.one.service
+
+import com.team.one.domain.SimulatorCommand
+
+interface InterestService {
+  def calculate(BigDecimal capital)
+}
---
+++ src/main/groovy/com/team/one/service/InterestServiceImpl.groovy
@@ -0,0 +1,15 @@
+package com.team.one.service
+
+import com.team.one.domain.SimulatorCommand
+import org.springframework.stereotype.Service
+import com.team.one.domain.PaymentPeriod
+import com.team.one.domain.Paydays
+import com.team.one.exception.SimulatorException
+
+@Service
+class InterestServiceImpl implements InterestService {
+
+  def calculate(BigDecimal capital){
+    command
+  }
+}
