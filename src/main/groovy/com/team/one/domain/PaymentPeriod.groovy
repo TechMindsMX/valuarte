a389ce65c8626fb5db02cfe89fd3e011cc5b4c71
josdem <joseluis.delacruz@gmail.com>
2015-10-01 12:03:37 -0500

    #63 Agregando el label al payment period

--- src/main/groovy/com/team/one/domain/PaymentPeriod.groovy
+++ src/main/groovy/com/team/one/domain/PaymentPeriod.groovy
@@ -1,23 +1,30 @@
 package com.team.one.domain
 
 enum PaymentPeriod {
-  WEEKLY("7,15,21,30",4),
-  FORTNIGHT("15,30",2),
-  MONTHLY("30",1)
+  WEEKLY("Semanal","7,15,21,30",4),
+  FORTNIGHT("Quincenal","15,30",2),
+  MONTHLY("Mensual","30",1)
 
+  private final String label
   private final String value
   private final Integer factor
 
-  PaymentPeriod(String value, Integer factor) {
+  PaymentPeriod(String label, String value, Integer factor) {
+    this.label = label
     this.value = value
     this.factor = factor
+  }
+
+  String getLabel() {
+    label
+  }
+
+  String getValue() {
+    value
   }
 
   Integer getFactor(){
     factor
   }
 
-  String getValue() {
-    value
-  }
 }
