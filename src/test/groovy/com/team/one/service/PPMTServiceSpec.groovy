b6ea79cb3e19f3ba94901a3f032571446da6cb39
josdem <joseluis.delacruz@gmail.com>
2015-10-06 11:51:32 -0500

    #63 Agregando mas casos de prueba

--- src/test/groovy/com/team/one/service/PPMTServiceSpec.groovy
+++ src/test/groovy/com/team/one/service/PPMTServiceSpec.groovy
@@ -35,6 +35,64 @@ class PPMTServiceSpec extends Specification {
     where:"We have next cases"
       numberPayment | principle     | tia     |  iva  | numberOfPayments | paymentPeriod            || result
       12            | 31732         | 40      |  16   | 12               | PaymentPeriod.MONTHLY    || 2128.04
+      11            | 31732         | 40      |  16   | 12               | PaymentPeriod.MONTHLY    || 2210.32
+      10            | 31732         | 40      |  16   | 12               | PaymentPeriod.MONTHLY    || 2295.79
+      9             | 31732         | 40      |  16   | 12               | PaymentPeriod.MONTHLY    || 2384.56
+      8             | 31732         | 40      |  16   | 12               | PaymentPeriod.MONTHLY    || 2476.76
   }
+
+  void "should throw an exception when no payment period"() {
+    given:"A simulator simulator and principle"
+      def simulator = new Simulator()
+    and:"Input values"
+      simulator.iva = 16
+      simulator.tia = 40
+      simulator.principle = 15000
+      simulator.numberOfPayments = 12
+      def numberPayment = 12
+    when:"We calculate data"
+      service.calculate(simulator, numberPayment)
+    then:"Thrown exception"
+      thrown SimulatorException
+  }
+
+  @Unroll
+  void """when we have tia: #tia and want to calculate we expect Exception"""() {
+    given:"A simulator simulator and principle"
+      def simulator = new Simulator()
+    and:"Input values"
+      simulator.iva = 16
+      simulator.principle = 15000
+      simulator.numberOfPayments = 12
+      simulator.paymentPeriod = PaymentPeriod.WEEKLY
+      simulator.tia = tia
+      def numberPayment = 12
+    when:"We calculate data"
+      def result = service.calculate(simulator, numberPayment)
+    then:"Thrown exception"
+      thrown SimulatorException
+    where:"We have next values"
+      tia << [null, 0, -1]
+  }
+
+  @Unroll
+  void """when we have principle: #principle and want to calculate we expect Exception"""() {
+    given:"A simulator simulator and principle"
+      def simulator = new Simulator()
+    and:"Input values"
+      simulator.iva = 16
+      simulator.tia = 40
+      simulator.numberOfPayments = 12
+      simulator.paymentPeriod = PaymentPeriod.WEEKLY
+      simulator.principle = principle
+      def numberPayment = 12
+    when:"We calculate data"
+      def result = service.calculate(simulator, numberPayment)
+    then:"Thrown exception"
+      thrown SimulatorException
+    where:"We have next values"
+      principle << [null, 0, -1]
+  }
+
 
 }
