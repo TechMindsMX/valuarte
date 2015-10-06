fa18532843bbca3b28bc553179421b73d7b975b1
says <says.rodriguez@gmail.com>
2015-10-06 10:32:51 -0500

    #69 se crea el command de las variables de la seccion de client

---
+++ src/main/groovy/com/team/one/command/ClientCommand.groovy
@@ -0,0 +1,25 @@
+package com.team.one.command
+
+import com.team.one.domain.enums.*
+
+class ClientCommand {
+	
+	String montoPrestamo
+  String nombre
+  String apellidoPaterno
+  String apellidoMaterno
+  String rfc
+  Date fechaNacimiento
+  Integer edad
+  EstadoCivil estadoCivil
+  RegimenMatrimonial regimen
+  GradoMaximoEstudios grado
+  String gradoOtro
+  Genero genero
+  TipoIdentificacion identificacion
+  String identificacionOtro
+  String fiel
+  String curp
+  String claveElector
+  String nacionalidad
+}
