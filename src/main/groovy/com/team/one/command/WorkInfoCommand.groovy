f4bcecb2c407266c306371b6ceac606c77d2a4a3
says <says.rodriguez@gmail.com>
2015-10-06 10:37:25 -0500

    coding #69 se agrega el command de la informacion laboral

---
+++ src/main/groovy/com/team/one/command/WorkInfoCommand.groovy
@@ -0,0 +1,17 @@
+package com.team.one.domain
+
+import com.team.one.domain.enums.*
+
+class WorkInfoCommand {
+
+	String profecion
+  String empresa
+  String giroActividad
+  TipoContrato contrato
+  Date fechaIngreso
+  String sueldoMensual
+  String domicilioEmpresa
+  String ciudadEstadoEmpresa
+  String telefonoEmpresa
+
+}
