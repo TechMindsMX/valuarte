ee7ec8c347aaa89aa2aa5aa6d19910b9f921eae7
says <says.rodriguez@gmail.com>
2015-10-06 10:35:00 -0500

    #69 se agrega el command con las propiedades de la informacion financiera

---
+++ src/main/groovy/com/team/one/command/FinancialInfoCommand.groovy
@@ -0,0 +1,35 @@
+package com.team.one.domain
+
+import com.team.one.domain.enums.*
+
+class FinancialInfoCommand {
+
+	Boolean creditoActual
+  Boolean avalCreditoTercero
+  String tipoCreditoDeuda1
+  String institucionDeuda1
+  String numeroCreditoDeuda1
+  Date fechaAperturaDeuda1
+  Date fechaTerminoDeuda1
+  String montoOtorgadoDeuda1
+  String tipoCreditoDeuda2
+  String institucionDeuda2
+  String numeroCreditoDeuda2
+  Date fechaAperturaDeuda2
+  Date fechaTerminoDeuda2
+  String montoOtorgadoDeuda2
+  String tipoCreditoDeuda3
+  String institucionDeuda3
+  String numeroCreditoDeuda3
+  Date fechaAperturaDeuda3
+  Date fechaTerminoDeuda3
+  String montoOtorgadoDeuda3
+  String tipoCreditoDeuda4
+  String institucionDeuda4
+  String numeroCreditoDeuda4
+  Date fechaAperturaDeuda4
+  Date fechaTerminoDeuda4
+  String montoOtorgadoDeuda4
+  String nombreBeneficiario
+	
+}
