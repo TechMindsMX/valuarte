2d58d454a10fc37925e4c0b0f04f02e009b99520
says <says.rodriguez@gmail.com>
2015-10-06 10:35:41 -0500

    #69 se agrega el command de las referencias

---
+++ src/main/groovy/com/team/one/command/ReferencesCommand.groovy
@@ -0,0 +1,23 @@
+package com.team.one.domain
+
+import com.team.one.domain.enums.*
+
+class ReferencesCommand {
+	
+	String nombreCompletoRefFamiliar
+  String parentescoFamiliar
+  String telefonoFamiliar
+  String domicilioFamiliar
+  String nombreCompletoRefLaboral
+  String parentescoLaboral
+  String telefonoLaboral
+  String domicilioLaboral
+  String nombreCompletoRefPersonal1
+  String parentescoPersonal1
+  String telefonoPersonal1
+  String domicilioPersonal1
+  String nombreCompletoRefPersonal2
+  String parentescoPersonal2
+  String telefonoPersonal2
+  String domicilioPersonal2
+}
