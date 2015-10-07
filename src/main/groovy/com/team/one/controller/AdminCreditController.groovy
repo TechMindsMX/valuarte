2fdd8710e98aa3c87361fdb6049f7898b54bcade
says <says.rodriguez@gmail.com>
2015-10-07 13:55:30 -0500

    #52 se ajusta el metodo save del controller para llamar al service create

--- src/main/groovy/com/team/one/controller/AdminCreditController.groovy
+++ src/main/groovy/com/team/one/controller/AdminCreditController.groovy
@@ -1,16 +1,23 @@
 package com.team.one.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.access.prepost.PreAuthorize
 import com.team.one.command.CreditCommand
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
+import com.team.one.domain.*
+import org.springframework.ui.Model
+import com.team.one.service.AdminCreditService
 
 @Controller
 @RequestMapping("/adminCredit")
 class AdminCreditController {
+
+  @Autowired
+  AdminCreditService service
 
   @PreAuthorize("hasAuthority('ADMIN')")
   @RequestMapping(value="/create",method=RequestMethod.GET)
@@ -19,12 +26,10 @@ class AdminCreditController {
     "adminCredit/form"
   }
 
-
   @RequestMapping(value="/save", method=RequestMethod.POST)
-  String save(CreditCommand command, BindingResult result) {
+  String save(CreditCommand command, BindingResult result,Model model) {
     Authentication auth = SecurityContextHolder.getContext().getAuthentication()
     String name = auth.getName()
-    println auth.getPrincipal()
     def address = command.addressCommand.generateAddress()
     def endorsement = command.endorsementCommand.generateEndorsement()
     def financialInfo = command.financialInfoCommand.generateFinancialInfo()
@@ -33,6 +38,19 @@ class AdminCreditController {
     def transactionalProfile = command.transactionalProfileCommand.generateTransactionalProfile()
     def workInfo = commad.workInfoCommand.generateWorkInfo()
     def client = command.clientCommand.generateClient()
+    List domainList = [
+                        address:address,
+                        endorsement:endorsement,
+                        financial:financialInfo,
+                        reference:references,
+                        sure:sure,
+                        transactional:transactionalProfile,
+                        work:workInfo,
+                       client:client
+                      ]
+
+    def clientPersistence = service.create(domainList,name)
+    model.aadAtribute("clientUuid",clientPersistence.uuid)
     "adminCredit/form"
   }
 
