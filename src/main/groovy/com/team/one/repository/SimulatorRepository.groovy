c121e47888f75f546008bb490612c8c17a72fe76
josdem <joseluis.delacruz@gmail.com>
2015-10-09 12:16:54 -0500

    #75 Changing to display recent simulator stored

--- src/main/groovy/com/team/one/repository/SimulatorRepository.groovy
+++ src/main/groovy/com/team/one/repository/SimulatorRepository.groovy
@@ -5,7 +5,4 @@ import com.team.one.domain.Simulator
 import org.springframework.data.mongodb.repository.MongoRepository
 
 public interface SimulatorRepository extends MongoRepository<Simulator, String> {
-
-  Simulator findById(String id)
-
 }
