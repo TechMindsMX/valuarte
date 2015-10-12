aa0699e2a35fbd25e92cb0e4a9f162065940cb46
says <says.rodriguez@gmail.com>
2015-10-12 08:30:30 -0500

    #35 se ajusta las condiciones de columns de nullable false a true

--- src/main/groovy/com/team/one/domain/FinancialInfo.groovy
+++ src/main/groovy/com/team/one/domain/FinancialInfo.groovy
@@ -10,85 +10,85 @@ class FinancialInfo {
   	@GeneratedValue(strategy=GenerationType.AUTO)
   	Long id
 
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Boolean creditoActual
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Boolean avalCreditoTercero
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String tipoCreditoDeuda1
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String institucionDeuda1
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String numeroCreditoDeuda1
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Date fechaAperturaDeuda1
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Date fechaTerminoDeuda1
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String montoOtorgadoDeuda1
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String tipoCreditoDeuda2
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String institucionDeuda2
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String numeroCreditoDeuda2
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Date fechaAperturaDeuda2
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Date fechaTerminoDeuda2
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String montoOtorgadoDeuda2
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String tipoCreditoDeuda3
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String institucionDeuda3
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String numeroCreditoDeuda3
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Date fechaAperturaDeuda3
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Date fechaTerminoDeuda3
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String montoOtorgadoDeuda3
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String tipoCreditoDeuda4
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String institucionDeuda4
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String numeroCreditoDeuda4
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Date fechaAperturaDeuda4
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	Date fechaTerminoDeuda4
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String montoOtorgadoDeuda4
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	String nombreBeneficiario
 
 }
