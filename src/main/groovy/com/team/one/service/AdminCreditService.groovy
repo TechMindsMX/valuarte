9e80be360284444052a43c45fe31e2a70013b9df
says <says.rodriguez@gmail.com>
2015-10-12 12:23:48 -0500

    #35 se agrega el parametro faltantes al metodo del create

--- src/main/groovy/com/team/one/service/AdminCreditService.groovy
+++ src/main/groovy/com/team/one/service/AdminCreditService.groovy
@@ -1,9 +1,10 @@
 package com.team.one.service
 
 import com.team.one.domain.Client
+import com.team.one.domain.User
 
 interface AdminCreditService {
 
-  Client create(List domainList,String userCreate)
+  Client create(List domainList,String userCreate,User user)
 
 }
