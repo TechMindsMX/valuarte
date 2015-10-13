a83ac6a7dfe6043ddafe6c7117a1547afa445da9
josdem <joseluis.delacruz@gmail.com>
2015-10-13 15:26:08 -0500

    #77 Pasando el test

--- src/main/groovy/com/team/one/service/impl/OpeningCommissionServiceImpl.groovy
+++ src/main/groovy/com/team/one/service/impl/OpeningCommissionServiceImpl.groovy
@@ -5,13 +5,25 @@ import com.team.one.service.OpeningCommissionService
 import org.springframework.stereotype.Service
 import com.team.one.domain.PaymentPeriod
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import java.math.RoundingMode
+
 import com.team.one.exception.SimulatorException
 
 @Service
 class OpeningCommissionServiceImpl implements OpeningCommissionService {
 
+  @Value('${simulator.decimals}')
+  Integer decimals
+  @Value('${simulator.roundingMode}')
+  String roundingMode
+
   BigDecimal calculate(Simulator simulator){
-    return 0
+    if(!simulator.loan){
+      throw new SimulatorException()
+    }
+
+    return (simulator.loan / (1-(simulator.openingCommission/100 * (1 + simulator.iva/100)))).setScale(decimals, RoundingMode.valueOf(roundingMode))
   }
 
 }
---
+++ src/test/groovy/com/team/one/service/OpeningCommissionServiceSpec.groovy
@@ -0,0 +1,36 @@
+package com.team.one.service
+
+import com.team.one.service.impl.OpeningCommissionServiceImpl
+import spock.lang.Specification
+import spock.lang.Unroll
+import com.team.one.domain.Simulator
+import com.team.one.exception.SimulatorException
+
+class OpeningCommissionServiceSpec extends Specification {
+
+  OpeningCommissionServiceImpl service = new OpeningCommissionServiceImpl()
+
+  Integer decimals = 2
+  String roundingMode = 'HALF_UP'
+
+  def setup(){
+    service.decimals = decimals
+    service.roundingMode = roundingMode
+  }
+
+  @Unroll
+  void """When we have an loan: #loan, openingCommission: #openingCommission, iva: #iva and we want to compute principle we expect: result: #result"""() {
+    given:"A simulator"
+      def simulator = new Simulator()
+    when:"We have this values"
+      simulator.loan = loan
+      simulator.openingCommission = openingCommission
+      simulator.iva = iva
+    then:"We calculate values"
+      result == service.calculate(simulator)
+    where:"We have next cases"
+    loan   | openingCommission | iva || result
+    31732  | 2                 | 16  || 32485.67
+  }
+
+}
