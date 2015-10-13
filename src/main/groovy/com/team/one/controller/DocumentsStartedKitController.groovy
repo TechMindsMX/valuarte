2a88d6fbfc5ee18a057bc4351ae2dde742652bfb
says <says.rodriguez@gmail.com>
2015-10-13 15:32:19 -0500

    #35 se ajustan y se agregan dependencias asi como el metodo de descarga del archivo

--- src/main/groovy/com/team/one/controller/DocumentsStartedKitController.groovy
+++ src/main/groovy/com/team/one/controller/DocumentsStartedKitController.groovy
@@ -3,7 +3,7 @@ package com.team.one.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.*
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,6 +11,11 @@ import org.springframework.web.servlet.ModelAndView
 import org.springframework.util.FileCopyUtils
 
 import com.team.one.service.DocumentService
+import com.team.one.repository.ClientRepository
+import org.springframework.ui.Model
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Controller
 @RequestMapping("/startKit")
@@ -18,16 +23,25 @@ class DocumentsStartedKitController {
 
   @Autowired
   DocumentService documentService
-    
+  @Autowired
+  ClientRepository repository
+  @Value('${valuarte.document.list.names}')
+  def listDocuments
+
   @PreAuthorize("hasAuthority('ADMIN')")
-  @RequestMapping(value="/create", method=RequestMethod.GET)
-  def createStartedKitOfCredit(def username) {
-    def file = documentService.createDocumentStartedKit(username)
-    def fis = new FileInputStream(file)
-    response.setContentType("application/pdf")
-    response.setContentLength(((int) file.size()))
-    response.setHeader("Content-Disposition","attachment filename=\"" + file.name +"\"")
-    FileCopyUtils.copy(fis, response.getOutputStream())
+  @RequestMapping(value="/create", method=RequestMethod.GET, produces =  "application/pdf")
+  @ResponseBody
+  byte[] createStartedKitOfCredit(String uuid) {
+    def client = repository.findByUuid(uuid.toString())
+    def file = documentService.createDocumentStartedKit(client,listDocuments)
+    file.bytes
+  }
+
+  @PreAuthorize("hasAuthority('ADMIN')")
+  @RequestMapping(value="/show",method=RequestMethod.GET)
+  def showAllClients(Model model) {
+    model.addAttribute("clients",repository.findAll())
+    "client/show"
   }
 
 }
