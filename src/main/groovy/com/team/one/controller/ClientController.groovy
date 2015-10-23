91011913cc88e800a249200386062467cba2ede7
josdem <joseluis.delacruz@gmail.com>
2015-10-23 18:46:09 -0500

    #103 Agregando la funcionalidad de busqueda por cliente en el controller

--- src/main/groovy/com/team/one/controller/ClientController.groovy
+++ src/main/groovy/com/team/one/controller/ClientController.groovy
@@ -1,10 +1,13 @@
 package com.team.one.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
+
+import com.team.one.service.ClientService
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -13,12 +16,16 @@ import org.slf4j.LoggerFactory
 @RequestMapping("/client")
 class ClientController {
 
+  @Autowired
+  ClientService clientService
+
   Logger log = LoggerFactory.getLogger(getClass())
 
   @RequestMapping(method=RequestMethod.GET)
   @ResponseBody
   def findClientByRfc(@RequestParam String rfc){
     log.info "FINDING Client by rfc: $rfc"
+    clientService.findByRfc(rfc)
   }
 
 }
